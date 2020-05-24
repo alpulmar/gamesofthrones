b54abe3701d22040b4fdfdaf1555eeda61166817
alpulmar <alpulmar@ucm.es>
2020-05-24 19:08:20 +0200

    ficheros modificados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ListPersonajesComponent } from './pages/pages/personajes/list-personajes/list-personajes.component';
+import { DetailPersonajeComponent } from './pages/pages/personajes/detail-personaje/detail-personaje.component';
+import { ListCasasComponent } from './pages/pages/casas/list-casas/list-casas.component';
+import { DetailCasaComponent } from './pages/pages/casas/detail-casa/detail-casa.component';
+import { CronologiaComponent } from './pages/pages/cronologia/cronologia.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'homepage',
+    component: HomepageComponent,
+  },
+  {
+    path: 'list-personajes',
+    component: ListPersonajesComponent,
+  },
+  {
+    path: 'detail-personaje',
+    component: DetailPersonajeComponent,
+  },
+  {
+    path: 'list-casas',
+    component: ListCasasComponent,
+  },
+  {
+    path: 'detail-casa',
+    component: DetailCasaComponent,
+  },
+  {
+    path: 'cronologia',
+    component: CronologiaComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full',
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/services/got.service.ts
+++ src/app/shared/services/got.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GotService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getListPersonajes() {
+    return this.httpClient.get(environment.url + 'pages/personajes/list-personajes');
+  }
+
+  getDetailPersonaje(name) {
+    return this.httpClient.get(environment.url + 'pages/personajes/detail-personaje'+name);
+  }
+
+  getCasas() {
+    return this.httpClient.get(environment.url + 'pages/casas/list-casas');
+  }
+
+  getDetailCasa(name) {
+    return this.httpClient.get(environment.url + 'pages/casas/detail-casa'+name);
+  }
+
+  getCronologia(name) {
+    return this.httpClient.get(environment.url + 'pages/cronologia'+name);
+  }
+
 }
