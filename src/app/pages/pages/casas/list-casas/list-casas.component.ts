336a5cfb2f98aea2799aa824b3d579b78f5f55e6
alpulmar <alpulmar@ucm.com>
2020-10-20 21:00:46 +0200

    ficheros modificados

--- src/app/pages/pages/casas/list-casas/list-casas.component.ts
+++ src/app/pages/pages/casas/list-casas/list-casas.component.ts
@@ -9,13 +9,30 @@ import { GotService } from '../../../../shared/services/got.service';
 
 export class ListCasasComponent implements OnInit {
 
-  ListCasas: Array<any>;
+  Casas: Array<any>;
+  searchCasa: string;
+
   constructor(private gotService: GotService) {}
 
   ngOnInit(): void {
     this.gotService.getCasas().subscribe((res: any) => {
       console.log(res);
-      this.ListCasas = res.splice(0, 15);
+      this.Casas = res.splice(0, 15);
     });
+  }
+
+  searchCasas() {
+    this.Casas = [];
+
+    this.gotService.getCasas().subscribe((houses: any) => {
+      this.Casas = houses.filter((item) => {
+        let cadena = item.name.toLowerCase();
+
+        if (cadena.indexOf(this.searchCasa.toLowerCase()) != -1) {
+          return item;
+        }
+      });
+    });
+    this.Casas;
   }
 }
--- src/app/pages/pages/personajes/list-personajes/list-personajes.component.ts
+++ src/app/pages/pages/personajes/list-personajes/list-personajes.component.ts
@@ -9,6 +9,8 @@ import { GotService } from '../../../../shared/services/got.service';
 export class ListPersonajesComponent implements OnInit {
 
   ListPersonajes: Array<any>;
+  searchPersonaje: string;
+  
   constructor(private gotService: GotService) {}
 
   ngOnInit(): void {
@@ -16,5 +18,20 @@ export class ListPersonajesComponent implements OnInit {
       console.log(res);
       this.ListPersonajes = res.splice(0, 15);
     });
+  }
+
+  searchListPersonajes() {
+    this.ListPersonajes = [];
+
+    this.gotService.getListPersonajes().subscribe((characters: any) => {
+      this.ListPersonajes = characters.filter((item) => {
+        let cadena = item.name.toLowerCase();
+
+        if (cadena.indexOf(this.searchPersonaje.toLowerCase()) != -1) {
+          return item;
+        }
+      });
+    });
+    this.ListPersonajes;
   }
 }
